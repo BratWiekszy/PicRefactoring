2042393ae0fd6056fec187563ac3845f503821ae
Brat Wiekszy <bratwiekszy@gmail.com>
2019-01-04 22:58:15 +0100

    Added RenameRegex rename method with tests.

---
+++ PicRefactoring.Tests/Actions/RenameRegexTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using PicRefactoring.Actions;
+
+namespace PicRefactoring.Tests.Actions
+{
+	[TestFixture]
+	public class RenameRegexTests
+	{
+		[Test]
+		public void GetRenamedFileName_CorrectResult()
+		{
+			var action = new RenameRegex(@"(?<=[\w]{3}).{1,15}", 9);
+			action.CheckValidity();
+
+			Assert.AreEqual("ename123", action.GetRenamedFileName("filename123"));
+			Assert.AreEqual("ename1234", action.GetRenamedFileName("filename1234"));
+			Assert.AreEqual("ename1234567890", action.GetRenamedFileName("filename1234567890-XDD"));
+		}
+	}
+}
---
+++ PicRefactoring/Actions/RenameRegex.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.RegularExpressions;
+using JsonRazor.Serialization;
+using PicRefactoring.Commanding;
+
+namespace PicRefactoring.Actions
+{
+	[JsonInfo(ModelMembers.Property, Flags = Deserializer.DefaultFlags)]
+	public sealed class RenameRegex : Rename
+	{
+		private Regex _regex;
+
+		public string OutputRegex { get; private set; }
+
+		public RenameRegex() {}
+
+		public RenameRegex(string outputRegex, int minLength) 
+		{
+			OutputRegex = outputRegex;
+			MinLength = minLength;
+		}
+
+		public override void CheckValidity()
+		{
+			base.CheckValidity();
+			if(OutputRegex.IsNullOrWhitespace())
+				throw new BadCommandException();
+
+			_regex = new Regex(OutputRegex);
+		}
+
+		public override string GetRenamedFileName(string fileName)
+		{
+			var match = _regex.Match(fileName);
+			return match.Value;
+		}
+	}
+}
