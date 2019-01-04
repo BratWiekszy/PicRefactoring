cb972eb652fc2d5fa6089c83c45a29dde4b3e7f7
Brat Wiekszy <bratwiekszy@gmail.com>
2019-01-04 17:59:04 +0100

    Added ActionEntry with tests.

---
+++ PicRefactoring.Tests/Commanding/ActionEntryTests.cs
@@ -0,0 +1,42 @@
+﻿using JsonRazor;
+using NUnit.Framework;
+using PicRefactoring.Commanding;
+
+namespace PicRefactoring.Tests.Commanding
+{
+	[TestFixture]
+	public class ActionEntryTests
+	{
+		[Test]
+		public void DetectsBadDeclaration()
+		{
+			Assert.Throws<BadCommandException>(() => new ActionEntry(null, null));
+			Assert.Throws<BadCommandException>(() => new ActionEntry("", null));
+			Assert.Throws<BadCommandException>(() => new ActionEntry("	", null));
+
+			Assert.Throws<BadCommandException>(() => new ActionEntry("rescale", null));
+			Assert.Throws<BadCommandException>(() => new ActionEntry("rename", null));
+			Assert.Throws<BadCommandException>(() => new ActionEntry("suggestDuplicates", null));
+
+			Assert.Throws<BadCommandException>(() => new ActionEntry("rescale", new object()));
+			Assert.Throws<BadCommandException>(() => new ActionEntry("rename", new object()));
+			Assert.Throws<BadCommandException>(() => new ActionEntry("suggestDuplicates", new object()));
+		}
+
+		[Test]
+		public void DetectsNotSupportedActionTypes()
+		{
+			Assert.Throws<BadCommandException>(() => new ActionEntry("scale", new JsonToken()));
+			Assert.Throws<BadCommandException>(() => new ActionEntry("Name", new JsonToken()));
+			Assert.Throws<BadCommandException>(() => new ActionEntry("duplicates", new JsonToken()));
+		}
+
+		[Test]
+		public void ActionTypesDeclarationAintTooStrict()
+		{
+			Assert.DoesNotThrow(() => new ActionEntry("Rescale",      new JsonToken()));
+			Assert.DoesNotThrow(() => new ActionEntry("rename",       new JsonToken()));
+			Assert.DoesNotThrow(() => new ActionEntry("SUGGESTDUPLICATES", new JsonToken()));
+		}
+	}
+}
---
+++ PicRefactoring/Commanding/ActionEntry.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using JsonRazor;
+using JsonRazor.Serialization;
+
+namespace PicRefactoring.Commanding
+{
+	public enum ActionType
+	{
+		Rescale,
+		Rename,
+		SuggestDuplicates
+	}
+
+	[JsonInfo(new Type[]{typeof(string), typeof(object)}, true, 
+			  nameof(ActionEntry.Type), nameof(ActionEntry.Value), Members = ModelMembers.None)]
+	public class ActionEntry
+	{
+		private ActionType _type;
+
+		public string Type { get; }
+		public object Value { get; }
+
+		public ActionEntry(string Type, object Value)
+		{
+			this.Type = Type;
+			this.Value = Value;
+
+			CheckGeneralValiditity();
+			ConvertType();
+		}
+
+		private void CheckGeneralValiditity()
+		{
+			if(Type.IsNullOrWhitespace())
+				throw new BadCommandException();
+
+			if(Value == null || !(Value is JsonToken))
+				throw new BadCommandException();
+		}
+
+		private void ConvertType()
+		{
+			if(Enum.TryParse<ActionType>(Type, true, out _type) == false)
+				throw new BadCommandException();
+		}
+	}
+}
