948f26faae35805ce73fda2d3b99bca4bd1673ea
Brat Wiekszy <bratwiekszy@gmail.com>
2019-01-04 22:59:07 +0100

    Added RescaleAction class.

---
+++ PicRefactoring/Actions/RescaleAction.cs
@@ -0,0 +1,19 @@
+﻿using JsonRazor.Serialization;
+using PicRefactoring.Commanding;
+
+namespace PicRefactoring.Actions
+{
+	[JsonInfo(ModelMembers.Property, Flags = Deserializer.DefaultFlags)]
+	public class RescaleAction : IFileAction
+	{
+		public const int MinSize = 32;
+
+		public int MaxSize { get; private set; }
+
+		public void CheckValidity()
+		{
+			if(MaxSize < MinSize)
+				throw new BadCommandException();
+		}
+	}
+}
