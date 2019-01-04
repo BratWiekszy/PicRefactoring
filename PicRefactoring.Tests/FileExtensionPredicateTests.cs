b0cb47f1f4ad7e3fdddb7bb26ab3ba2a8bba7bae
Brat Wiekszy <bratwiekszy@gmail.com>
2019-01-04 13:14:13 +0100

    Added FileExtensionPredicate with tests.

---
+++ PicRefactoring.Tests/FileExtensionPredicateTests.cs
@@ -0,0 +1,55 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using PicRefactoring.Abstractions;
+using PicRefactoring.Commanding;
+using PicRefactoring.Predicates;
+
+namespace PicRefactoring.Tests
+{
+	[TestFixture()]
+	public class FileExtensionPredicateTests
+	{
+		[TestCase("jpg")]
+		[TestCase("png")]
+		[TestCase("jpeg")]
+		public void FileMatches_YieldsPositive(string ext)
+		{
+			var predicate = new FileExtensionPredicate(new[] { "jpg", "png", "jpeg" });
+			var file = Substitute.For<IFileWrapper>();
+			file.GetExtension().Returns(ext);
+
+			Assert.True(predicate.FileMatches(file));
+		}
+
+		[TestCase("mp4")]
+		[TestCase("p1ng")]
+		[TestCase("tiff")]
+		public void FileMatches_YieldsNegative(string ext)
+		{
+			var predicate = new FileExtensionPredicate(new[] { "jpg", "png", "jpeg" });
+			var file      = Substitute.For<IFileWrapper>();
+			file.GetExtension().Returns(ext);
+
+			Assert.False(predicate.FileMatches(file));
+		}
+
+		[TestCase("jpg")]
+		[TestCase("png")]
+		[TestCase("jpeg")]
+		public void FileMatches_MatchingAintTooStrict(string ext)
+		{
+			var predicate = new FileExtensionPredicate(new[] { "PNG", ".jpg", ".JPEG" });
+			var file      = Substitute.For<IFileWrapper>();
+			file.GetExtension().Returns(ext);
+
+			Assert.True(predicate.FileMatches(file));
+		}
+
+		[Test]
+		public void DetectsBadExtensions()
+		{
+			Assert.Throws<BadCommandException>(() => new FileExtensionPredicate(new[] { "", null, "	" }));
+		}
+	}
+}
---
+++ PicRefactoring/Predicates/FileExtensionPredicate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using PicRefactoring.Abstractions;
+using PicRefactoring.Commanding;
+
+namespace PicRefactoring.Predicates
+{
+	public class FileExtensionPredicate : IFilePredicate
+	{
+		private string[] _extensions;
+
+		public FileExtensionPredicate(string[] extensions) 
+		{
+			_extensions = extensions;
+
+			CheckValidity();
+			CleanExtensions();
+		}
+
+		private void CheckValidity()
+		{
+			if(_extensions.Any(e => string.IsNullOrEmpty(e)))
+				throw new BadCommandException();
+		}
+
+		private void CleanExtensions()
+		{
+			_extensions = _extensions.Select(e =>
+			{
+				if(e.StartsWith('.'))
+					return e.Substring(1).ToLower();
+
+				return e.ToLower();
+			}).ToArray();
+		}
+
+		public bool FileMatches(IFileWrapper file)
+		{
+			var ext = file.GetExtension().ToLower();
+			return _extensions.Contains(ext);
+		}
+	}
+}
