3e9ee9621a41a3024daf38cc50f7c70d49012469
Brat Wiekszy <bratwiekszy@gmail.com>
2019-01-04 14:16:11 +0100

    Added FileNamePredicate with tests

---
+++ PicRefactoring.Tests/FileNamePredicateTests.cs
@@ -0,0 +1,47 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using PicRefactoring.Abstractions;
+using PicRefactoring.Commanding;
+using PicRefactoring.Predicates;
+
+namespace PicRefactoring.Tests
+{
+	[TestFixture()]
+	public class FileNamePredicateTests
+	{
+		[Test]
+		public void BadRegexesAreDetected()
+		{
+			Assert.Throws<BadCommandException>(() => new FileNamePredicate(new[] { "", null, "	" }));
+		}
+		
+		[TestCase("filename1234567")]
+		[TestCase("0afilename1234567890")]
+		[TestCase(@"__arima_senne_original_drawn_by_kashiwamochi_yomogi__sample-254b3de30c3a8abf49c1adfa1a0d8493")]
+		public void FileMatches_YieldsPositive(string fileName)
+		{
+			var predicate = new FileNamePredicate(
+					new[] { @"^[\w\d]{15,}$", @"^[\d][\w]{20,}$", @"^__([\w_\d]+)[_-]{1,2}sample[-_]{1,2}[\dabcdef]+$" }
+				);
+			var file      = Substitute.For<IFileWrapper>();
+			file.GetFileName().Returns(fileName);
+
+			Assert.True(predicate.FileMatches(file));
+		}
+
+		[TestCase("filename1")]
+		[TestCase("0afile_-name_1234567890")]
+		[TestCase(@"__arima_senne_original_drawn_by%%_kashiwamochi&_yomogi")]
+		public void FileMatches_YieldsNegative(string fileName)
+		{
+			var predicate = new FileNamePredicate(
+				new[] { @"^[\w\d]{15,}$", @"^[\d][\w]{20,}$", @"^__([\w_\d]+)[_-]{1,2}sample[-_]{1,2}[\dabcdef]+$" }
+			);
+			var file = Substitute.For<IFileWrapper>();
+			file.GetFileName().Returns(fileName);
+
+			Assert.False(predicate.FileMatches(file));
+		}
+	}
+}
---
+++ PicRefactoring/Predicates/FileNamePredicate.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+using JetBrains.Annotations;
+using PicRefactoring.Abstractions;
+using PicRefactoring.Commanding;
+
+namespace PicRefactoring.Predicates
+{
+	public sealed class FileNamePredicate : IFilePredicate
+	{
+		private string[] _regExpressions;
+		private Regex[] _regexes;
+
+		public FileNamePredicate([NotNull] string[] regExpressions) 
+		{
+			_regExpressions = regExpressions;
+
+			CheckValidity();
+			Prepare();
+		}
+
+		private void CheckValidity()
+		{
+			if(_regExpressions.Any(r => string.IsNullOrEmpty(r)))
+				throw new BadCommandException();
+		}
+
+		private void Prepare()
+		{
+			_regexes = _regExpressions.Select(e => new Regex(e)).ToArray();
+		}
+
+		public bool FileMatches(IFileWrapper file)
+		{
+			var fileName = file.GetFileName();
+			return _regexes.Any(r => r.IsMatch(fileName));
+		}
+
+	}
+}
