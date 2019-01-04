a01a255a07e83c4152eb444203b5a398329bc0bb
Brat Wiekszy <bratwiekszy@gmail.com>
2019-01-04 22:47:38 +0100

    Added RenameRandom rename mode with tests.

---
+++ PicRefactoring.Tests/Actions/RenameRandomTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using PicRefactoring.Actions;
+
+namespace PicRefactoring.Tests.Actions
+{
+	[TestFixture]
+	public class RenameRandomTests
+	{
+		[Test]
+		public void GetRenamedFileName_CorrectResult()
+		{
+			var action = new RenameRandom(9);
+			action.CheckValidity();
+
+			var name = action.GetRenamedFileName("filename123");
+			Assert.NotNull(name);
+			Assert.AreEqual(9, name.Length);
+
+			var name2 = action.TryMakeFileNameUnique(name, "filename123");
+			Assert.AreNotEqual(name, name2);
+		}
+	}
+}
---
+++ PicRefactoring/Actions/RenameRandom.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using JsonRazor.Serialization;
+
+namespace PicRefactoring.Actions
+{
+	[JsonInfo(ModelMembers.Property, Flags = Deserializer.DefaultFlags)]
+	public sealed class RenameRandom : Rename
+	{
+		public RenameRandom() {}
+
+		public RenameRandom(int minLength)
+		{
+			MinLength = minLength;
+		}
+
+		public override string GetRenamedFileName(string fileName)
+		{
+			var name = Path.GetRandomFileName();
+			if (name.Length <= MinLength)
+				return name;
+
+			return name.Substring(0, MinLength);
+		}
+	}
+}
