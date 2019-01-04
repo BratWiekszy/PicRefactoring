8ef600c201c87f78c4c6d3539ea173fe0cfd7687
Brat Wiekszy <bratwiekszy@gmail.com>
2019-01-04 22:36:58 +0100

    Added base class for renaming modes.

---
+++ PicRefactoring/Actions/Rename.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using PicRefactoring.Commanding;
+
+namespace PicRefactoring.Actions
+{
+	public abstract class Rename
+	{
+		public const int MinimalLength = 4;
+
+		protected Random _random;
+
+		public int MinLength { get; protected set; }
+
+		protected Rename()
+		{
+			_random = new Random();
+		}
+
+		public virtual void CheckValidity()
+		{
+			if(MinLength < MinimalLength)
+				throw new BadCommandException();
+		}
+
+		public abstract string GetRenamedFileName(string fileName);
+
+		public virtual string TryMakeFileNameUnique(string fileName, string originalFileName)
+		{
+			var randomChar = (char)('0' + _random.Next() % 10);
+			return fileName + randomChar.ToString();
+		}
+	}
+}
