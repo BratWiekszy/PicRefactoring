bf43891b2f5fa9084100e36c4ac9ae7ad82cbec2
Brat Wiekszy <bratwiekszy@gmail.com>
2019-01-04 22:38:01 +0100

    Added RenameCut rename mode with tests.

---
+++ PicRefactoring.Tests/Actions/RenameCutTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using PicRefactoring.Actions;
+
+namespace PicRefactoring.Tests.Actions
+{
+	[TestFixture]
+	public class RenameCutTests
+	{
+		[Test]
+		public void GetRenamedFileName_CorrectResult()
+		{
+			var action = new RenameCut(9);
+			action.CheckValidity();
+
+			Assert.AreEqual("file_name", action.GetRenamedFileName("file_name123456789"));
+			Assert.AreEqual("file_name", action.GetRenamedFileName("file_name"));
+			Assert.AreEqual("filename", action.GetRenamedFileName("filename"));
+
+			var fn = action.GetRenamedFileName("file_name_&_123456789");
+			fn = action.TryMakeFileNameUnique(fn, "file_name_&_123456789");
+			fn = action.TryMakeFileNameUnique(fn, "file_name_&_123456789");
+			fn = action.TryMakeFileNameUnique(fn, "file_name_&_123456789");
+			Assert.AreEqual("file_name_&_", fn);
+		}
+	}
+}
---
+++ PicRefactoring/Actions/RenameCut.cs
@@ -0,0 +1,34 @@
+﻿using JsonRazor.Serialization;
+
+namespace PicRefactoring.Actions
+{
+	[JsonInfo(ModelMembers.Property, Flags = Deserializer.DefaultFlags)]
+	public sealed class RenameCut : Rename
+	{
+		public RenameCut() {}
+
+		public RenameCut(int minLength)
+		{
+			MinLength = minLength;
+		}
+
+		public override string TryMakeFileNameUnique(string fileName, string originalFileName)
+		{
+			var nameLength = fileName.Length;
+			if (nameLength < originalFileName.Length)
+			{
+				char nextSrcChar = originalFileName[nameLength];
+				return fileName + nextSrcChar.ToString();
+			}
+			return base.TryMakeFileNameUnique(fileName, originalFileName);
+		}
+
+		public override string GetRenamedFileName(string fileName)
+		{
+			if (fileName.Length <= MinLength)
+				return fileName;
+
+			return fileName.Substring(0, MinLength);
+		}
+	}
+}
