e6abe44052b8a3ff6498eab5bcafd097e8578cab
Brat Wiekszy <bratwiekszy@gmail.com>
2019-01-10 00:50:16 +0100

    Added final command model class with tests.

---
+++ PicRefactoring.Tests/Commanding/CommandsTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using PicRefactoring.Commanding;
+
+namespace PicRefactoring.Tests.Commanding
+{
+	[TestFixture]
+	public class CommandsTests
+	{
+		[Test]
+		public void CheckValidity_ThrowsOnInvalidDirPaths()
+		{
+			Assert.Throws<BadCommandException>(() => new Commands(new[] { @"??><>.-&^%$#@!" }, null).CheckValidity());
+			Assert.Throws<BadCommandException>(() 
+				=> new Commands(new[] {@"c:\txt.txt", @"??><>\\txt.png" }, null).CheckValidity());
+
+			Assert.Throws<BadCommandException>(() => new Commands(new[] { @"d:\elo.jpg" }, null).CheckValidity());
+		}
+	}
+}
---
+++ PicRefactoring/Commanding/Commands.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System.Linq;
+using JetBrains.Annotations;
+using JsonRazor.Serialization;
+
+namespace PicRefactoring.Commanding
+{
+	[JsonInfo(ModelMembers.Property, Flags = Deserializer.DefaultFlags)]
+	public class Commands
+	{
+		public string[]         Directories { get; private set; }
+		public ExecutionEntry[] Executions  { get; private set; }
+
+		private Commands() {}
+
+		public Commands(string[] directories, ExecutionEntry[] executions)
+		{
+			Directories = directories;
+			Executions = executions;
+		}
+
+		public void CheckValidity()
+		{
+			if(Directories == null || Directories.Length == 0)
+				throw new BadCommandException();
+
+			if(Directories.Any(d => IsInvalidPath(d)))
+				throw new BadCommandException();
+
+			if(Executions == null || Executions.Length == 0)
+				throw new BadCommandException();
+		}
+
+		private bool IsInvalidPath(string path)
+		{
+			return path.IsNullOrWhitespace()
+				|| path.IndexOfAny(Path.GetInvalidPathChars()) >= 0 
+				|| path.IndexOfAny(Path.GetInvalidFileNameChars()) >= 0;
+		}
+
+		[NotNull]
+		public Execution[] CreateExecutions()
+		{
+			return Executions.Select(e => e.CreateExecution()).ToArray();
+		}
+	}
+}
