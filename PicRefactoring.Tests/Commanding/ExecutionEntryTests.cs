278400e2fa72c303c0092294d858068099d464f5
Brat Wiekszy <bratwiekszy@gmail.com>
2019-01-09 17:55:21 +0100

    Added ExecutionEntry with tests

---
+++ PicRefactoring.Tests/Commanding/ExecutionEntryTests.cs
@@ -0,0 +1,50 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using PicRefactoring.Commanding;
+using PicRefactoring.Predicates;
+
+namespace PicRefactoring.Tests.Commanding
+{
+	[TestFixture]
+	public class ExecutionEntryTests 
+	{
+
+		class Test : ExecutionEntry
+		{
+			public Test(PredicateEntry[] predicates, ActionEntry[] actions) : base(predicates, actions) {}
+
+			public IFilePredicate[] RunGetOrderedPredicates()
+			{
+				return base.GetOrderedPredicates();
+			} 
+		}
+
+		[Test]
+		public void CreateExecution_ThrowsIfNoPredicateOrAction()
+		{
+			Assert.Throws<BadCommandException>(() => new ExecutionEntry().CreateExecution());
+			Assert.Throws<BadCommandException>(() => 
+					new ExecutionEntry(new PredicateEntry[0], new ActionEntry[0]).CreateExecution());
+			Assert.Throws<BadCommandException>(() => 
+					new ExecutionEntry(new PredicateEntry[0], null).CreateExecution());
+		}
+
+		[Test]
+		public void CreateExecution_EnsureCorrectOrder()
+		{
+			var pred1 = Substitute.For<PredicateEntry>();
+			var pred2 = Substitute.For<PredicateEntry>();
+			var fp1 = Substitute.For<IFilePredicate>();
+			var fp2 = Substitute.For<IFilePredicate>();
+			pred1.CreatePredicate().Returns(fp1);
+			pred2.CreatePredicate().Returns(fp2);
+			fp1.Order.Returns(2);
+			fp2.Order.Returns(1);
+
+			var entry = new Test(new []{pred1,pred2}, new []{Substitute.For<ActionEntry>()});
+
+			var correctOrder = new IFilePredicate[] { fp2, fp1 };
+			CollectionAssert.AreEqual(correctOrder, entry.RunGetOrderedPredicates());
+		}
+	}
+}
---
+++ PicRefactoring/Commanding/ExecutionEntry.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using JsonRazor.Serialization;
+using PicRefactoring.Actions;
+using PicRefactoring.Predicates;
+
+namespace PicRefactoring.Commanding
+{
+	[JsonInfo(ModelMembers.Property, Flags = Deserializer.DefaultFlags)]
+	public class ExecutionEntry
+	{
+		public string           Title      { get; private set; }
+		public PredicateEntry[] Predicates { get; private set; }
+		public ActionEntry[]    Actions    { get; private set; }
+
+		public ExecutionEntry() {}
+
+		public ExecutionEntry(PredicateEntry[] predicates, ActionEntry[] actions)
+		{
+			Predicates = predicates;
+			Actions = actions;
+		}
+
+		public Execution CreateExecution()
+		{
+			CheckValidity();
+
+			var predicates = GetOrderedPredicates();
+			var actions = GetActions();
+			return new Execution(predicates, actions, this);
+		}
+
+		protected IFilePredicate[] GetOrderedPredicates() => 
+			Predicates.Select(p => p.CreatePredicate()).OrderBy(p => p.Order).ToArray();
+
+		protected IFileAction[] GetActions() => 
+			Actions.Select(p => p.CreateAction(null)).ToArray();
+
+		private void CheckValidity()
+		{
+			if(Predicates == null || Predicates.Length == 0)
+				throw new BadCommandException();
+
+			if(Actions == null || Actions.Length == 0)
+				throw new BadCommandException();
+
+			if(Predicates.Any(p => p == null))
+				throw new BadCommandException();
+
+			if(Actions.Any(a => a == null))
+				throw new BadCommandException();
+		}
+	}
+}
