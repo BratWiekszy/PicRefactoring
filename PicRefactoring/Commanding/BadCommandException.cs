9d3c331f4ae664a37a91b59795175660a225fd35
Brat Wiekszy <bratwiekszy@gmail.com>
2019-01-10 20:10:12 +0100

    Added some describing methods

--- PicRefactoring/Commanding/BadCommandException.cs
+++ PicRefactoring/Commanding/BadCommandException.cs
@@ -4,5 +4,12 @@ namespace PicRefactoring.Commanding
 {
 	public class BadCommandException : Exception
 	{
+		public BadCommandException() {}
+
+		public BadCommandException(string message) 
+			: base("Invalid data: "+ message) {}
+
+		public BadCommandException(string message, BadCommandException innerException) 
+			: base(message + Environment.NewLine + innerException.Message, innerException) {}
 	}
 }
